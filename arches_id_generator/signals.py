92327beaea27554b425777c56187c7ad11a54a61
Stuart Marshall <stuart.marshall@flaxandteal.co.uk>
2026-05-06 12:58:05 +0100

    fix: correctly update string for i18n

--- arches_id_generator/signals.py
+++ arches_id_generator/signals.py
@@ -1,8 +1,10 @@
+from django.conf import settings
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+from django.utils.translation import get_language
 
 from arches.app.models.models import CardXNodeXWidget
-from arches.app.models.tile import Tile
+from arches.app.models.tile import Tile  # noqa: F401
 
 from arches_id_generator.constants import WIDGET_ID
 from arches_id_generator.services.generator import generate_id
@@ -15,18 +17,21 @@ def assign_id_to_tile(sender, instance, **kwargs):
     )
     
     for entry in cxnxws:
-        node_id = entry.node_id
+        node_id = str(entry.node_id)
         sequence_key = entry.config.get("sequence_key")
         template = entry.config.get("template")
-        
+
         if not sequence_key or not template:
             continue
-        
+
         current = instance.data.get(node_id)
-        
+
         if current:
             continue
-        
+
         new_value = generate_id(sequence_key, template)
-        instance.data[node_id] = new_value
+        language = get_language() or settings.LANGUAGE_CODE
+        instance.data[node_id] = {
+            language: {"value": new_value, "direction": "ltr"},
+        }
         
