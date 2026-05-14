d7cae1af6209932e7c6d5ad162dae7edfec472d6
Stuart Marshall <stuart.marshall@flaxandteal.co.uk>
2026-05-14 16:29:08 +0100

    feat: generate_on config + uuid/random tokens in widget

--- arches_id_generator/media/js/views/components/widgets/id-generator-widget.js
+++ arches_id_generator/media/js/views/components/widgets/id-generator-widget.js
@@ -4,29 +4,31 @@ import arches from "arches";
 import idGeneratorTemplate from "templates/views/components/widgets/id-generator-widget.htm";
 
 const viewModel = function(params) {
-    params.configKeys = ["sequence_key", "template", "default_placeholder", "auto_populate"];
+    params.configKeys = [
+        "sequence_key",
+        "template",
+        "default_placeholder",
+        "auto_populate",
+        "generate_on",
+    ];
     WidgetViewModel.apply(this, [params]);
     const self = this;
 
     self.displayValue = ko.computed(() =>
         ko.unwrap(ko.unwrap(self.value)?.[arches.activeLanguage]?.value) ?? ""
     );
-    console.log("[id-generator] init", {
-        configKeys: self.configKeys,
-        config: ko.toJS(self.config),
-        auto_populate_isObservable: ko.isObservable(self.auto_populate),
-        auto_populate_initialValue: ko.unwrap(self.auto_populate),
-        hasDirtyWidget: typeof params.graphDesignerHasDirtyWidget,
-    });
+
+    const markDirty = () => {
+        if (typeof params.graphDesignerHasDirtyWidget === "function") {
+            params.graphDesignerHasDirtyWidget(true);
+        }
+    };
 
     if (ko.isObservable(self.auto_populate)) {
-        self.auto_populate.subscribe((val) => {
-            console.log("[id-generator] auto_populate changed →", val,
-                "config now:", ko.toJS(self.config));
-            if (typeof params.graphDesignerHasDirtyWidget === "function") {
-                params.graphDesignerHasDirtyWidget(true);
-            }
-        });
+        self.auto_populate.subscribe(markDirty);
+    }
+    if (ko.isObservable(self.generate_on)) {
+        self.generate_on.subscribe(markDirty);
     }
 };
 
